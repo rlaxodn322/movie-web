6fc7bacae24f9d7dcee7c44b5ff938ac905d25bb
taewoo <rlaxodn322@gmail.com>
2023-09-20 15:35:47 +0900

    영화webPage(commit)

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const IMG_URL = "https://image.tmdb.org/t/p/w1280/";
+export const IMG_URL = "https://image.tmdb.org/t/p/w1280/";
 
-export default function Movie({ title, poster_path, vote_average }) {
+export default function Movie(props) {
+  const navigate = useNavigate();
+  const onClickMovieItem = () => {
+    navigate(`/movie/${props.title}`, { state: props });
+  };
   return (
-    <div className="movie-container">
-      <img src={IMG_URL + poster_path} alt="영화포스터" />
+    <div className="movie-container" onClick={onClickMovieItem}>
+      <img src={IMG_URL + props.poster_path} alt="영화포스터" />
       <div className="movie-info">
-        <h4>{title}</h4>
-        <span>{vote_average}</span>
+        <h4>{props.title}</h4>
+        <span>{props.vote_average}</span>
       </div>
     </div>
   );
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useParams, useLocation } from "react-router-dom";
+import { IMG_URL } from "../components/Movie";
 
 export default function MovieDetail() {
+  const { title } = useParams();
+  const { state } = useLocation();
+
   return (
-    <div className="page-container" style={{ fontSize: "32px" }}>
-      MovieDetail
+    <div className="page-container">
+      <div style={{ display: "flex" }}>
+        <img
+          style={{ width: "300px", height: "450px" }}
+          src={IMG_URL + state.poster_path}
+          alt="영화 포스터 이미지"
+        />
+        <div>
+          <div style={{ fontSize: "32px" }}>{title}</div>
+        </div>
+      </div>
     </div>
   );
 }
